e6406f145fbeb1c0752066a58433e5a9cf7dcccb
akashunni <akashunni123@gmail.com>
2026-04-07 09:38:19 +0530

    chore: update llama.cpp to b8683

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let llamaVersion = "b8638"
-let llamaChecksum = "7d7d44e35550ebf5ac803173f1897d9dd3dd9a5f8d44218559228cfe966399b7"
+let llamaVersion = "b8683"
+let llamaChecksum = "6b5b408d68bdf38b429dee098439da21f3f2fe972d49dc745aa10b951c2b14c0"
 
 let package = Package(
     name: "swift-llama-cpp",
